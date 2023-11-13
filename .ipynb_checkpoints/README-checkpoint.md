ad76ee1cf566599ef36afb62c04a556fc624752f
LEONOB2014 <aleonardobecerra@gmail.com>
2023-11-12 21:40:40 -0500

    config docker-compose

---
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -0,0 +1,2 @@
+# Data-engineering-zoomcamp
+My personal work for the Data Engineering Zoomcamp Course from Datatalks
--- Week_1_basics_n_setup/3_docker_postgres/ingest_data2.py
+++ Week_1_basics_n_setup/3_docker_postgres/ingest_data2.py
@@ -16,14 +16,16 @@ def main(params):
     table_name = params.table_name
     url = params.url
 
-    parquet_name = 'output.parquet'
+    csv_name = 'output.csv'
 
     # doenload the parquet file
-    os.system(f"wget {url} -O {parquet_name}")
+    os.system(f"wget {url} -O {csv_name}")
 
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
 
-    df = pd.read_parquet(parquet_name)
+    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=10000)
+
+    df=next(df_iter)
 
     df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
     df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
@@ -34,6 +36,15 @@ def main(params):
     df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
     df.to_sql(name=table_name, con=engine, if_exists='append')
     
+    while True:
+        t_start = time()
+        df = next(df_iter)
+        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+        df.to_sql(name=table_name, con=engine, if_exists='append')
+        t_end = time()
+        print('inserted another chunk, too %.3f second' % (t_end - t_start))
+
 
 if __name__ == '__main__':
         
